2513243d9386e893d38ad87a7a68f065a0a1184e
Ana <anarodic93@gmail.com>
2015-06-10 18:16:37 +0200

    IT250-DZ07-Ana_Rodic_1565

---
+++ src/main/java/com/metropolitan/methotels/entities/Drzava.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.metropolitan.methotels.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+/**
+ *
+ * @author Ana
+ */
+@Entity
+@Table(name = "DRZAVA", catalog = "methotels", schema = "")
+@NamedQueries({
+    @NamedQuery(name = "Drzava.findAll", query = "SELECT d FROM Drzava d")})
+public class Drzava implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "ime_drzave")
+    private String imeDrzave;
+
+    @Inject
+    public Drzava() {
+    }
+
+    public Drzava(Integer id) {
+        this.id = id;
+    }
+
+    public Drzava(Integer id, String imeDrzave) {
+        this.id = id;
+        this.imeDrzave = imeDrzave;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImeDrzave() {
+        return imeDrzave;
+    }
+
+    public void setImeDrzave(String imeDrzave) {
+        this.imeDrzave = imeDrzave;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Drzava)) {
+            return false;
+        }
+        Drzava other = (Drzava) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return imeDrzave;
+    }
+
+}
